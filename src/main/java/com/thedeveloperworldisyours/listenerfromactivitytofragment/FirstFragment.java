50a7dc2400c07113813caa4b3a9bd907e70a3989
javierG <javierg@Administrators-Mac-Pro-4.local>
2016-05-24 09:48:56 +0100

    first commit

---
+++ src/main/java/com/thedeveloperworldisyours/listenerfromactivitytofragment/FirstFragment.java
@@ -0,0 +1,60 @@
+package com.thedeveloperworldisyours.listenerfromactivitytofragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link FirstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FirstFragment extends Fragment {
+
+    public TextView mTextView;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static FirstFragment newInstance() {
+        FirstFragment fragment = new FirstFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        mTextView = (TextView) view.findViewById(R.id.fragment_blank_text);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public void refreshString(String string) {
+        mTextView.setText(string);
+    }
+}
---
+++ src/main/java/com/thedeveloperworldisyours/listenerfromactivitytofragment/MainActivity.java
@@ -0,0 +1,97 @@
+package com.thedeveloperworldisyours.listenerfromactivitytofragment;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private MenuItem mSearchMenuItem;
+
+    private SearchView mSearchView;
+    private String mSearchString;
+    private static final String SEARCH_KEY = "search";
+
+    FirstFragment mFirstFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // if you saved something on outState you can recover them here
+        if (savedInstanceState != null) {
+            mSearchString = savedInstanceState.getString(SEARCH_KEY);
+        }
+        mFirstFragment = new FirstFragment().newInstance();
+        replaceFragment();
+    }
+
+    // This is called before the activity is destroyed
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mSearchString = mSearchView.getQuery().toString();
+        outState.putString(SEARCH_KEY, mSearchString);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mSearchMenuItem = menu.findItem(R.id.menu_main_action_search);
+
+        mSearchView = (SearchView) MenuItemCompat.getActionView(mSearchMenuItem);
+
+        customSearView();
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mFirstFragment.refreshString(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void customSearView() {
+        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) mSearchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchAutoComplete.setHintTextColor(Color.WHITE);
+        searchAutoComplete.setTextColor(Color.WHITE);
+
+        View searchPlate = mSearchView.findViewById(android.support.v7.appcompat.R.id.search_plate);
+        searchPlate.setBackgroundResource(R.drawable.background_search);
+
+        mSearchView.setIconifiedByDefault(false);
+    }
+
+
+    public void replaceFragment() {
+
+        try {
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_main, mFirstFragment, "tag").commit();
+
+        } catch (Exception e) {
+            Log.d(TAG, e.toString());
+        }
+
+    }
+
+
+}
